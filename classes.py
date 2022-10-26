e2d469983e0fa0db81247b1a2e186f675aef33ad
Vitor Angheben <vitor_angheben@hotmail.com>
2022-10-25 22:36:33 -0300

    I've finshed all the functions and learn the QR Code Now, put the QR Code in the payment function

--- classes.py
+++ classes.py
@@ -1,10 +1,10 @@
 class Game:
-    def __init__(self, name, time_played, players, average_time, price):
+    def __init__(self, name, time_played, players, price):
         self.name = name
         self.time_played = time_played
         self.players = players
-        self.average_time = average_time
         self.price = price
 
     def __str__(self):
-        return f"The game {self.name}, was played "
+        return f"The game {self.name}, was played {self.time_played} hours by his users, there are {self.players} " \
+               f"players and it costs around US$ {self.price}!"
--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pandas as pd
-import qrcode
+import qrcode_test
+
+df_game_list = pd.DataFrame(pd.read_excel("game_list.xlsx", sheet_name="game_list"))
+shopping_cart = []
+price_shopping_cart = []
 
 
 def menu():
@@ -28,22 +32,46 @@ def menu():
 
 
 def see_the_products():
-    pass
+    print(df_game_list)
+    menu()
 
 
 def add_product_shopping_cart():
-    pass
+    global add_shopping_cart
+    try:
+        add_shopping_cart = int(input("Select the ID of the product that you want to add at your shopping cart\n"
+                                      "-> "))
+    except ValueError as err1:
+        print("ID not found, please try again")
+        add_product_shopping_cart()
+    else:
+        print(f"The product {df_game_list[add_shopping_cart]['Product']} has been added to your shopping cart")
+        shopping_cart.append(df_game_list[add_shopping_cart]['Product'])
+        print(shopping_cart)
+        menu()
 
 
 def check_shopping_cart():
-    pass
+    print(pd.DataFrame(shopping_cart))
 
 
 def delete_product_shopping_cart():
-    pass
+    global id_to_delete
+    try:
+        id_to_delete = int(input("Type the ID of the product that you want to delete of your shopping cart:\n"
+                                 "-> "))
+    except ValueError as err2:
+        print(f"The product of ID {id_to_delete} is not in your shopping cart, try again")
+        delete_product_shopping_cart()
+    else:
+        shopping_cart.remove(id_to_delete)
+        menu()
 
 
 def payment():
+
+    # Learn how to put the price of the spreadsheet in a QR Code
+
     pass
 
 
