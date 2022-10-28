4e2d8220ea2a7a0caf711e865c473c133d9813c3
Vitor Angheben <vitor_angheben@hotmail.com>
2022-10-28 16:29:55 -0300

    Finally it's done!!

---
+++ test.py
@@ -0,0 +1,11 @@
+dictionary = {1: ['vaca', 2],
+              2: ['boi', 14]}
+
+for key, value in dictionary.items():
+    print(value[1])
+
+"""
+lista = [1, 2, 3, 4]
+for v in lista:
+    print(lista[1])
+"""
